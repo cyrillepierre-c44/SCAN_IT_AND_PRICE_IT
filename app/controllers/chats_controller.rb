4c03d0c4fa8a6718741d39de1d9b99e3310421b3
marc thomas <marc.thomas5608@gmail.com>
2026-03-03 17:35:17 +0100

    chatshow

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,5 +4,17 @@ class ChatsController < ApplicationController
   end
 
   def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to request_path(@chat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:title, :user_id, :request_id)
   end
 end
