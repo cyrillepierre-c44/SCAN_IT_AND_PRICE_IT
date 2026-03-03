0052d3f3298e825eaccf43b4f155a2405b6238aa
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-03 15:22:36 +0100

    seeds ok, schema corrigé

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :chats, dependent: :destroy
   validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
   validates :password, presence: true, length: { minimum: 6 }
