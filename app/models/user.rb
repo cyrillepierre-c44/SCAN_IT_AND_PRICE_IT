f7e91b0c6a82e2e254b2b3dc4e10e09ad1a9acc2
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-03 11:56:41 +0100

    correction user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :chats, dependant: destroy
+  has_many :chats, dependant: :destroy
   validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
   validates :password, presence: true, length: { minimum: 6 }
 end
