7c2e791845a07dcd91154b64577ad97d22b60707
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-04 12:22:17 +0100

    llm

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,3 +1,7 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPENAI_API_KEY"]
+  config.openai_api_key = ENV["GITHUB_KEY"]
+  config.openai_api_base = "https://models.inference.ai.azure.com"
+  response = chat.ask "Describe this image.", with: { image: "tmp/lewagon-student.png" }
+  puts response.content
+  # ... see RubyLLM configuration guide for other models
 end
